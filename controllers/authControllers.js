78516650e2608a065e499fe73976b3bdef810691
Tglowria <tomiglows@gmail.com>
2024-10-02 02:51:01 +0100

    added delete nd profile pic controller

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const cloudinary = require('../public/cloudinary.js');
 const jwt = require('jsonwebtoken');
 const { v4: uuidv4 } = require("uuid");
 const User = require("../models/userModels.js");
@@ -10,10 +11,10 @@ exports.personalSignup = async (req, res) => {
         if (!personalName || !email || !password || !country) {
             return res.status(400).json({ message: "Please provide all required fields" });
         }
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/;
+        const passwordRegex = /^(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
         if (!passwordRegex.test(password)) {
             return res.status(400).json({
-                message: "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+                message: "Password must be at least 8 characters long and must contain one special character.."
             });
         }
 
@@ -39,10 +40,10 @@ console.log(newUser);
   
         await newUser.save();
 
-        await emailSender(newUser);
-        await sendSms(newUser);
+        // await emailSender(newUser);
+        // await sendSms(newUser);
 
-        return res.status(201).json({ message: "User saved successfully", token, newUser });
+        return res.status(201).json({ message: "User saved successfully", newUser });
     } catch (err) {
         console.error(err);
         return res.status(500).json({ message: "Error saving user", error: err.message });
@@ -56,10 +57,10 @@ exports.bussinessSignup = async (req, res) => {
         if (!personalName || !businessName || !role || !industry || !email || !password || !country) {
             return res.status(400).json({ message: "Please provide all required fields" });
         }
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/;
+        const passwordRegex = /^(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
         if (!passwordRegex.test(password)) {
             return res.status(400).json({
-                message: "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+                message: "Password must be at least 8 characters long and must contain one special character."
             });
         }
 
@@ -76,55 +77,57 @@ exports.bussinessSignup = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, saltRounds);
 
         const newUser = new User({
-            firstName,
-            lastName,
+            personalName,
             password: hashedPassword,
             email,
-            phoneNumber,
-            otp,
+            country,
+            businessName,
+            role,
+            industry
         });
 console.log(newUser);
   
         await newUser.save();
 
-        await emailSender(newUser);
+        // await emailSender(newUser);
 
-        return res.status(201).json({ message: "User saved successfully", token, newUser });
+        return res.status(201).json({ message: "User saved successfully", newUser });
     } catch (err) {
         console.error(err);
         return res.status(500).json({ message: "Error saving user", error: err.message });
     }
 };
 exports.login = async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      if (!email || !password) {
-        return res
-          .status(400)
-          .json({ message: "Please input your email and password" });
-      }
-  
-      const user = await User.findOne({ email });
-  
-      if (!user) {
-        return res.status(404).json({ message: "User Not Found, Please Signup" });
-      }
-  
-      const correctPassword = await bcrypt.compare(password, user.password);
-      if (!correctPassword) {
-        return res.status(400).json({ message: "Incorrect Password" });
-      }
-  
-      const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, { expiresIn: '30s' });
-  
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
       return res
-        .status(200)
-        .json({ message: "User is Logged In Succesfully", user, token });
-    } catch (err) {
-      console.error(err);
-      return res.status(500).json({ message: "Error Logging In User", err });
+        .status(400)
+        .json({ message: "Please input your email and password" });
     }
-  };
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found, Please Signup" });
+    }
+
+    const correctPassword = await bcrypt.compare(password, user.password);
+    if (!correctPassword) {
+      return res.status(400).json({ message: "Incorrect Password" });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '30s' });
+
+    return res
+      .status(200)
+      .json({ message: "User is Logged In Successfully", user, token });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Error Logging In User", err });
+  }
+};
+
 
   exports.forgotPassword = async (req, res) => {
     try {
@@ -143,9 +146,9 @@ exports.login = async (req, res) => {
       user.resetPasswordToken = token;
       await user.save();
   
-      await emailResetPassword(email, token);
+      // await emailResetPassword(email, token);
   
-      return res.status(200).json({ message: "Check your email to reset your password" });
+      return res.status(200).json({ message: "Check your email to reset your password", });
     } catch (err) {
       console.error(err);
       return res.status(500).json({ message: "Error saving user", err });
@@ -181,5 +184,50 @@ exports.login = async (req, res) => {
     } catch (err) {
       console.error(err);
       return res.status(500).json({ message: "Error resetting password", err });
+    }
+  };
+
+  exports.uploadPicture = async (req, res) => {
+    try {
+    
+        const user = await User.findById(req.params.id);
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const result = await cloudinary.uploader.upload(req.file.path);
+
+        const updatedTrain = await Train.findByIdAndUpdate(
+            req.params.id,
+            { image: result.secure_url },
+            { new: true } 
+        );
+
+        return res.status(200).json({
+            message: "Picture uploaded successfully",
+            data: updatedTrain,
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Error uploading picture", error: err });
+    }
+};
+
+
+  exports.deleteUser = async (req, res) => {
+    try {
+      const { id } = req.query;
+  
+      const deletedUser = await User.findByIdAndDelete({
+        _id: id,
+      });
+  
+      res.status(200).json({
+        message: "User Deleted Successfully",
+        deletedUser,
+      });
+    } catch (error) {
+      res.status(500).json({ error, message: "Error Deleting User" });
     }
   };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { personalSignup, bussinessSignup, login, resetPassword, forgotPassword } = require('../controllers/authControllers');
+const { personalSignup, bussinessSignup, login, resetPassword, forgotPassword, deleteUser, uploadPicture } = require('../controllers/authControllers');
+const upload = require("../public/multer");
 
 const router = express.Router();
 
@@ -8,6 +9,8 @@ router.post('/bussinessSignup', bussinessSignup );
 router.post('/login', login );
 router.post('/reset/:token', resetPassword );
 router.post('/forgotpassword', forgotPassword );
+router.put('/picture/:id', upload.single("picture"), uploadPicture );
+router.delete('/delete', deleteUser);
 
 
 module.exports = router;
