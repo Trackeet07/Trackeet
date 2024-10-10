1877cd2649bca7eef061f2c17ef1a454a1fd5d0b
Tglowria <tomiglows@gmail.com>
2024-10-10 11:14:39 +0100

    update

--- models/expensesModels.js
+++ models/expensesModels.js
@@ -1,21 +1,20 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    personalName: {
+    expenseName: {
         type: String,
         required: true
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true
+    amount: {
+        type: Number,
+        required: true
     },
-    password: {
+    category: {
         type: String,
         required: true
     },
-    country: {
-        type: String,
+    date: {
+        type: Date,
         required: true
     },
     role: {
