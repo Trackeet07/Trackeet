1e9980098931c6e43f433b51b882c1f5867cdac8
Olaxy13 <olaxy13@gmail.com>
2024-12-07 23:53:24 -0800

    signup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,16 +1,23 @@
 import express, { json } from 'express';
-import { personalSignup, protect, businessSignup, loginUser, loginBusiness,   resetUserPassword, resetBusinessPassword, forgotUserPassword, forgotBusinessPassword,  uploadPicture, deleteUser, verifyUserEmail, verifyBusinessEmail } from '../controllers/authControllers.js';
+//import { personalSignup, protect, businessSignup, loginUser, loginBusiness,   resetUserPassword, resetBusinessPassword, forgotUserPassword, forgotBusinessPassword,  uploadPicture, deleteUser, verifyUserEmail, verifyBusinessEmail } from '../controllers/authControllers.js';
+// protect, initiatePayment residentApplication, 
+import { personalSignup,  businessSignup, loginUser, loginBusiness,  resetUserPassword, resetBusinessPassword, forgotUserPassword, forgotBusinessPassword,  uploadPicture, deleteUser, verifyUserEmail, verifyBusinessEmail, } from '../controllers/authControllers.js';
+import { residentSchema, validateRequest } from '../validation/validation.js';
+
 
 import upload from '../public/multer.js';
 
 
-const router = express.Router();
+const router = express.Router();  
 
-router.post('/personal', personalSignup );
+router.post('/signup', personalSignup );
 router.post('/bussiness', businessSignup);
 router.post('/verify-user-email', verifyUserEmail)
 router.post('/verify-business-email', verifyBusinessEmail)
 router.post('/login-user', loginUser );
+//router.post("/resident", validateRequest(residentSchema), residentApplication)
+
+//router.post("/residentPayment", initiatePayment )
 router.post('/login-business', loginBusiness );
 router.post('/reset-user', resetUserPassword );
 router.post('/reset-business', resetBusinessPassword );
