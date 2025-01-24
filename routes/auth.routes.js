f2667f7fafebe59c14cf72ca743f34e0463b5a00
Olaxy13 <olaxy13@gmail.com>
2025-01-24 03:33:04 -0800

    set new password

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express, { json } from 'express';
-import { personalSignup,  businessSignup, loginUser, loginBusiness,  resetUserPassword, resetBusinessPassword, forgotUserPassword, forgotBusinessPassword,  uploadPicture, uploadBusinessPicture, deleteUser, verifyUserEmail, verifyBusinessEmail, deleteBusiness} from '../controllers/authControllers.js';
-import { validateRequest, registerSchema, businessSchema, expenseSchema, budgetSchema, loginSchema, forgetPassSchema, updatePassword, resetPasswordSchema  } from '../validation/validation.js';
+import { personalSignup,  businessSignup, loginUser, loginBusiness,  resetUserPassword, resetBusinessPassword, forgotUserPassword, forgotBusinessPassword,  uploadPicture, uploadBusinessPicture, deleteUser, verifyUserEmail, verifyBusinessEmail, deleteBusiness, setNewUserPassword, setNewBusinessPassword} from '../controllers/authControllers.js';
+import { validateRequest, registerSchema, businessSchema, expenseSchema, budgetSchema, loginSchema, forgetPassSchema, updatePassword, resetPasswordSchema,   } from '../validation/validation.js';
 import isAuthenticated from "../middleware/auth.js"
 import validatePictureUpload from "../middleware/picValid.js"
 import upload from '../public/multer.js';
-
+import User from "../models/userModels.js"
+import Business from "../models/businessModel.js"
 
 const router = express.Router(); 
 
@@ -15,8 +16,11 @@ router.post('/login-user', validateRequest(loginSchema), loginUser );
 router.post('/forgot-user-password', validateRequest(forgetPassSchema), forgotUserPassword );
 router.post('/reset-user', resetUserPassword );
 router.delete('/delete/:id', deleteUser);
-router.post('/picture', isAuthenticated, upload.single("picture"), uploadPicture );
- //Business
+router.post('/picture', isAuthenticated(User), upload.single("picture"), uploadPicture );
+router.put('/set-new-user-password', isAuthenticated(User), validateRequest(updatePassword), setNewUserPassword);
+ 
+
+//Business
 router.post('/business', validateRequest(businessSchema), businessSignup);
 router.post('/verify-business-email', verifyBusinessEmail)
 router.post('/login-business', validateRequest(loginSchema), loginBusiness );
@@ -24,6 +28,6 @@ router.post('/reset-business', resetBusinessPassword );
 router.post('/forgot-business-password', validateRequest(forgetPassSchema), forgotBusinessPassword );
 router.delete('/delete-business/:id', deleteBusiness);
 router.post('/business-picture/:id', upload.single("picture"), uploadBusinessPicture );
-
+router.put('/set-new-business-password', isAuthenticated(Business), validateRequest(updatePassword), setNewBusinessPassword);
 
 export default router;
