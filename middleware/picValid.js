369bdab6c7e193be32d9ec3be00aa4722c60ce8f
Oke Olamide <olaxy13@gmail>
2025-04-24 16:25:07 +0100

    Updated

--- middleware/picValid.js
+++
@@ -1,7 +0,0 @@
-const validatePictureUpload = (req, res, next) => {
-    if (!req.file) {
-      return res.status(400).json({ message: "No picture file provided" });
-    }
-    next();
-  };
-  export default validatePictureUpload;
--- services/index.js
+++
@@ -1,4 +0,0 @@
-import emailService from './email.services.js';
-import tokenService from './token.service.js';
-
-export { emailService, tokenService }; 
